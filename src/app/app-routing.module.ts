5373762c4ef1c4dd60fa1e48c80233416b06b5af
arnaldorcm <arnaldo1ooo95@gmail.com>
2022-01-24 18:49:18 -0300

    Creacion de Clase Global y Clase Departamento.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', pathMatch:'full', redirectTo: 'departamentos'},
+  {
+    path: 'departamentos',
+    loadChildren: () => import('./departamentos/departamentos.module').then(m => m.DepartamentosModule)
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/departamentos/departamentos-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DepartamentosComponent } from './departamentos/departamentos.component';
+
+const routes: Routes = [
+  {path: '', component: DepartamentosComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class DepartamentosRoutingModule { }
