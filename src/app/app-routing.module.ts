c31b32baabbe78c281adf185bcd668d455999ad1
arnaldorcm <arnaldo1ooo95@gmail.com>
2023-01-02 12:01:57 -0300

    Para que el home se la raiz de la app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/containers/home/home.component';
 
 
 
 const routes: Routes = [
 
-  { path: '',  pathMatch: 'full', redirectTo: 'login' },  //Cuando path es vacio, para que redireccione al login
+  //{ path: '',  pathMatch: 'full', redirectTo: 'login' },  //Cuando path es vacio, para que redireccione al login
+  { path: '', component: HomeComponent },  //Raiz de la app
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'filiales', loadChildren: () => import('./filiales/filiales.module').then(m => m.FilialesModule) }
