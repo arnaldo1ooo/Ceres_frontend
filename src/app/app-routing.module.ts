8bf7421dc9f2c013fd7311dd075235b776321bff
arnaldorcm <arnaldo1ooo95@gmail.com>
2023-01-03 15:01:59 -0300

    Se retira LoginModule de los imports de app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,6 @@ import { LoginModule } from './login/login.module';
 //Aca se encuentras las rutas del proyecto, el CanActivate el guard (guardian o vigilante) verifica si existe un token almacenado, caso sea falso redirige al login
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full', canActivate: [AuthGuard] }, // Carga EAGER //Cuando path es vacio, redirecciona al home, caso tenga token almacenado (CanActivate)
-  { path: '**', component: LoginModule }, //Caso el path no coincida con ninguno redirigira al Login
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }, //Carga asincrona (LAZY Loading) requiere que el componente tenga un routing
   { path: 'home', canActivate: [AuthGuard], canLoad: [AuthGuard], loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'filiales', canActivate: [AuthGuard], canLoad: [AuthGuard], loadChildren: () => import('./filiales/filiales.module').then(m => m.FilialesModule) }
@@ -17,8 +16,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes),
-    LoginModule],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
