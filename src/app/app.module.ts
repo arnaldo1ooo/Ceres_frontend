f58189b63c87db493d240a7f784c15bb9f24e40b
arnaldorcm <arnaldo1ooo95@gmail.com>
2023-07-27 22:21:56 -0400

    Se crea componente de spinner cargando, para que se muestre mientras se realiza la peticion http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthInterceptor } from './autenticacion/interceptors/auth.interceptor';
+
 import { CompartidoModule } from './compartido/compartido.module';
+import { SpinnerCargandoInterceptor } from './compartido/componentes/spinner-cargando/interceptors/spinner-cargando.interceptor';
 import { FORMATO_REGIONAL_ES } from './compartido/constantes/constantes';
 import { MaterialModulosModule } from './compartido/material-modulos/material-modulos.module';
+import { SpinnerCargandoModule } from './compartido/componentes/spinner-cargando/spinner-cargando.module';
 
 registerLocaleData(localeEs, FORMATO_REGIONAL_ES);
 
@@ -27,10 +30,12 @@ registerLocaleData(localeEs, FORMATO_REGIONAL_ES);
     MatToolbarModule,
     HttpClientModule,
     CompartidoModule,
-    MaterialModulosModule
+    MaterialModulosModule,
+    SpinnerCargandoModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }, //Sirve para el interceptor de autenticacion helpers/autenticacion
+    { provide: HTTP_INTERCEPTORS, useClass: SpinnerCargandoInterceptor, multi: true }, //Sirve para que muestre cargando durante una peticion http
     { provide: LOCALE_ID, useValue: FORMATO_REGIONAL_ES } // Configura el LOCALE_ID a 'es' para usar el español como idioma
   ],
   bootstrap: [AppComponent]
