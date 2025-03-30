db4d9470deca22916b0385760310a03d1bc032e4
ARNALDO_CANTERO\arnal <arnaldo1ooo95@gmail.com>
2025-03-30 20:24:49 -0300

    Se crea form de orden inicial

--- src/app/modulos/ordenes/containers/ordenes-form/ordenes-form.component.ts
+++ src/app/modulos/ordenes/containers/ordenes-form/ordenes-form.component.ts
@@ -1,12 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map, Observable, of, startWith } from 'rxjs';
+import { HelpersService } from 'src/app/compartido/services/helpers.service';
+import { MercaderiaDTO } from 'src/app/modulos/mercaderias/model/dtos/mercaderiaDTO';
+import { MercaderiaListaDTO } from 'src/app/modulos/mercaderias/model/dtos/mercaderiaListaDTO';
+import { MercaderiasService } from 'src/app/modulos/mercaderias/services/mercaderias.service';
 
 @Component({
   selector: 'app-ordenes-form',
-  standalone: true,
-  imports: [],
+  standalone: false,
   templateUrl: './ordenes-form.component.html',
   styleUrl: './ordenes-form.component.scss'
 })
-export class OrdenesFormComponent {
+export class OrdenesFormComponent implements OnInit {
 
+  protected listaMercaderias: MercaderiaListaDTO[] = [];
+  protected listaMercaderiasFiltradas: MercaderiaListaDTO[] = [];
+  protected selectedItems: any[] = [];
+  protected filtroBuscarProducto: string = '';
+
+  constructor(
+    private _mercaderiasService: MercaderiasService
+  )
+  {
+
+  }
+
+  ngOnInit(): void {
+    this.cargarMercaderias();
+  }
+
+  cargarMercaderias() {
+       this._mercaderiasService.listarTodosMercaderiasActivos().subscribe({
+         next: (retorno: MercaderiaListaDTO[]) => {
+           this.listaMercaderias = retorno;
+           this.listaMercaderiasFiltradas = this.listaMercaderias;
+         },
+         error: (err) => console.log("Error al listar mercaderias: " + err)
+       });
+  }
+
+  onSearch() {
+
+    this.listaMercaderiasFiltradas = this.listaMercaderias.filter(merc =>
+      merc.descripcion.toLowerCase().includes(''.toLowerCase())
+    );
+  }
+
+  addItem(item: MercaderiaDTO) {
+    const existingItem = this.selectedItems.find(selected => selected.nombre === item.descripcion);
+    if (existingItem) {
+      existingItem.cantidad++;
+    } else {
+      this.selectedItems.push({...item, cantidad: 1});
+    }
+  }
+
+  removeItem(selectedItem: any) {
+    this.selectedItems = this.selectedItems.filter(item => item.nombre !== selectedItem.nombre);
+  }
+
+  getSubtotal() {
+    return this.selectedItems.reduce((total, item) => total + (item.precio * item.cantidad), 0);
+  }
+
+  getTax() {
+    return this.getSubtotal() * 0.15;
+  }
+
+  getTotal() {
+    return this.getSubtotal() + this.getTax();
+  }
+
+  checkout() {
+    alert('Checkout complete!');
+    // Implement checkout logic
+  }
 }
